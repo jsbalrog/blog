187ce9753248187a04968f9fb7e11b2ba4d46228
jsbalrog <edtjen@gmail.com>
2019-11-14 06:35:13 -0700

    setting routes programmatically

--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import { Link } from 'react-router-dom';
 
 import Post from '../../../components/Post/Post';
 import './Posts.css';
@@ -33,20 +32,23 @@ class Posts extends Component {
   }
 
   postSelectedHandler = (id) => {
-    this.setState({selectedPostId: id})
+    this.props.history.push({pathname: '/' + id});
   }
 
   render() {
     let posts = <p style={{textAlign: "center", color: "red"}}>Something went wrong</p>
     if(!this.state.error) {
         posts = this.state.posts.map(post => {
-            return (<Link key={post.id} to={'/' + post.id}>
-                    <Post  
-                      title={post.title} 
-                      author={post.author} 
-                      clicked={() => this.postSelectedHandler(post.id)}
-                      />
-                    </Link>)
+          return (
+            //<Link key={post.id} to={'/' + post.id}>
+            <Post
+                key={post.id}
+                title={post.title}
+                author={post.author}
+                clicked={() => this.postSelectedHandler(post.id)}
+                />
+            //</Link>
+          )
         });
     }
     return (
