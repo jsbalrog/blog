06f04e7194732e29508c8e3bb7774c3b7444c00f
jsbalrog <edtjen@gmail.com>
2019-11-15 13:31:37 -0700

    redirection

--- src/containers/Blog/NewPost/NewPost.js
+++ src/containers/Blog/NewPost/NewPost.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { Redirect } from 'react-router-dom';
 
 import './NewPost.css';
 
@@ -18,17 +19,24 @@ class NewPost extends Component {
         const post = {
             title: this.state.title,
             body: this.state.content,
-            author: this.state.author
+            author: this.state.author,
+            submitted: false,
         }
         axios.post('https://jsonplaceholder.typicode.com/posts', post)
             .then(response => {
                 console.log(response);
+                this.setState({ submitted: true });
             })
     }
 
     render () {
+        let redirect = null;
+        if(this.state.submitted) {
+            redirect = <Redirect to="/posts" />
+        }
         return (
             <div className="NewPost">
+                { redirect }
                 <h1>Add a Post</h1>
                 <label>Title</label>
                 <input type="text" value={this.state.title} onChange={(event) => this.setState({title: event.target.value})} />
