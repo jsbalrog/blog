b73ebfce4892f3f51b291a815602006104ebc3b9
jsbalrog <edtjen@gmail.com>
2019-11-17 18:18:31 -0700

    lazy load components base on routing

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
 import { Route, NavLink, Switch, Redirect }from 'react-router-dom';
 import Posts from './Posts/Posts'
-import NewPost from './NewPost/NewPost';
+// import NewPost from './NewPost/NewPost';
 import './Blog.css';
+import asyncComponent from '../../hoc/asyncComponent';
+
+const AsyncNewPost = asyncComponent(() => {
+  return import('./NewPost/NewPost');
+});
 
 class Blog extends Component {
 
@@ -23,7 +28,7 @@ class Blog extends Component {
                     </nav>
                 </header>
                 <Switch>
-                  <Route path="/new-post" component={NewPost}/>
+                  <Route path="/new-post" component={AsyncNewPost}/>
                   <Route path="/posts" component={Posts}/>
                   <Redirect from="/" to="/posts" />
                 </Switch>
